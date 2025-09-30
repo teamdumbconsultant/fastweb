862a9088c9e0d02f22ddd7f71015a7738282e5a3
teamdumbconsultant <teamdumbconsultant@gmail.com>
2025-09-30 03:10:18 -0400

    updated

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -5,7 +5,7 @@ const PrivacyPolicyPage = () => {
         <h1 className="text-4xl font-bold text-primary-text mb-6">Privacy Policy</h1>
         <div className="space-y-6 text-primary-text">
           <p>
-            Your privacy is important to us. It is AJayji's policy to respect your
+            Your privacy is important to us. It is AJayji&apos;s policy to respect your
             privacy regarding any information we may collect from you across our
             website, and other sites we own and operate.
           </p>
@@ -13,7 +13,7 @@ const PrivacyPolicyPage = () => {
           <p>
             We only ask for personal information when we truly need it to provide a
             service to you. We collect it by fair and lawful means, with your
-            knowledge and consent. We also let you know why we're collecting it and
+            knowledge and consent. We also let you know why we&apos;re collecting it and
             how it will be used.
           </p>
           <h2 className="text-3xl font-bold pt-4">How We Use Your Information</h2>
